5430dc884b7200d7cf0171a9bb228e959b72465e
Ernest Falcik <ernest.falcik@halopowered.com>
2022-05-12 17:28:06 +0300

    Laikinas BE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
@@ -18,7 +19,8 @@ import { NavigationComponent } from './navigation/navigation.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly httpClient: HttpClient
+  ) {
 
-  ngOnInit(): void {
   }
+
+  public ngOnInit(): void {
+
+
+     this.getTest();
+
+  }
+
+  public ngOnDestroy(): void {
+
+  }
+
+  public getTest(): Subscription {
+    return this.httpClient.get(`/1`).subscribe((test) => {
+      console.log(test);
+    });
+  }
+
 
 }
