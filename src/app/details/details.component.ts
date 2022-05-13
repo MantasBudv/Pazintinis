a3bfb2689348b71dea75453d44a775a0ddf8e350
Arunas Vaitkus <arunas.vaitkus@halopowered.com>
2022-05-13 12:18:56 +0300

    disable broken images

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -127,5 +127,8 @@ export class DetailsComponent implements OnInit, OnDestroy {
     });
   }
 
-
+  changeStyles(event: any) {
+    const el = event.target;
+    el.setAttribute('style', 'display: none');
+  }
 }
